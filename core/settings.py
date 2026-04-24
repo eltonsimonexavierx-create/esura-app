defa1f3af25ca7f3b729d70e405ea1422e90b38c
Elton <eltonsimonexavierx@gmail.com>
2026-04-24 15:19:59 +0200

    Forçando reconhecimento de ficheiros estáticos

--- core/settings.py
+++ core/settings.py
@@ -1,19 +1,11 @@
 import os
 from pathlib import Path
 
-# Caminho base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SEGURANÇA: Chave secreta para desenvolvimento
-SECRET_KEY = 'django-insecure-chave-acad-esura-2026'
-
-# DEBUG deve ser True para testes e False em produção real
-DEBUG = True
-
-# Permitir que a Vercel e dispositivos móveis acedam ao link
+SECRET_KEY = 'django-insecure-chave-esura-2026'
+DEBUG = True # Mantemos True para veres o erro caso a imagem falte
 ALLOWED_HOSTS = ['*']
 
-# Aplicações instaladas
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -23,7 +15,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-# Middlewares (WhiteNoise adicionado na segunda posição para servir o logotipo)
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -32,54 +23,32 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
 
-# Configuração de Templates
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
     },
-]
+}]
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# Base de Dados (SQLite para o início do sistema académico)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Configuração de Ficheiros Estáticos (Logotipo e CSS)
+# CONFIGURAÇÃO DE ESTÁTICOS - O FOCO É A TUA PASTA FÍSICA
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-# Modo de compatibilidade máxima para imagens na Vercel
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
 
-
-
-# Configuração vital para o logotipo aparecer na Vercel
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 

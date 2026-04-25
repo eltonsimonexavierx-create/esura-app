a2151858d8a5306ec8680095aed0f2bf5af84668
Elton <eltonsimonexavierx@gmail.com>
2026-04-25 16:50:10 +0200

    Fix: Registada a rota logout no urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('estudante/', views.estudante_view, name='estudante_dash'),
     path('professor/', views.professor_view, name='professor_dash'),
     path('admin-esura/', views.admin_view, name='admin_dash'),
+    # ESTA LINHA É A QUE FALTA PARA O ERRO DESAPARECER:
+    path('logout/', views.logout_view, name='logout'), 
 ]
+
+

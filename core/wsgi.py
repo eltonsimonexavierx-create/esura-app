d4bea8e47fae5ec69c1c0abd5240d4101a136f49
Elton <eltonsimonexavierx@gmail.com>
2026-04-25 23:00:34 +0200

    Adicionado gitignore e base de dados com usuários

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,9 +1,16 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Força o Python a encontrar a pasta core
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
-app = application  # <--- ADICIONA ESTA LINHA EXATAMENTE ASSIM
+app = application # Variável que a Vercel procura
+
 
 

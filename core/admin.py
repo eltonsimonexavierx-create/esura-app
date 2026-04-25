9d78c90ee98a61b3a255be096700acad9c155908
Elton <eltonsimonexavierx@gmail.com>
2026-04-25 17:13:26 +0200

    Fix: App registada no settings e modelos no admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Disciplina, Turma, Nota
+admin.site.register(Disciplina)
+admin.site.register(Turma)
+admin.site.register(Nota)
 
 @admin.register(Disciplina)
 class DisciplinaAdmin(admin.ModelAdmin):

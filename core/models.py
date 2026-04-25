5c155c3917bc45aa3cce6b3ba3723f72df027ccc
Elton <eltonsimonexavierx@gmail.com>
2026-04-25 14:13:30 +0200

    Sistema: Adicionados views, models e urls para perfis

---
+++ core/models.py
@@ -0,0 +1,5 @@
+from django.db import migrations, models
+
+# Por agora, usaremos os modelos padrão do Django para Usuários e Grupos.
+# Se no futuro precisares de uma tabela específica para 'Notas' ou 'Disciplinas',
+# será aqui que as vamos criar.
--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,9 @@
-from django.contrib import admin
 from django.urls import path
-from django.shortcuts import render
-from django.conf import settings
-from django.conf.urls.static import static
-
-def login_view(request):
-    return render(request, 'login.html')
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', login_view, name='login'),
+    path('', views.login_view, name='login'),
+    path('estudante/', views.estudante_view, name='estudante_dash'),
+    path('professor/', views.professor_view, name='professor_dash'),
+    path('admin-esura/', views.admin_view, name='admin_dash'),
 ]
-
-# Esta linha força o Django a encontrar as imagens na pasta static
-if settings.DEBUG or True:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
